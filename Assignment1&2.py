ee4e9c3d723a33245060d1b4684ecd950e2d8ffb
arizsalman <muhammadarizsalman@gmail.com>
2025-08-01 21:40:01 +0500

    Add

---
+++ Assignment1&2.py
@@ -0,0 +1,15 @@
+# print(" Hello Ariz ") agar ap ne english me kuch bhe lekna ho "Is ke ader add karo ge " e.g ===>
+
+print(" Hello Ariz ")
+
+# kio number lekhna ho Normal leky ge
+print(10)
+
+# or kese bhe value ko Double jetni par add  karna ke lekvana ho ====>
+print("Ariz"*8)
+
+# Variable This are premated type 
+any_words= "ariz" #this are string 
+any_number =122 #  this are number type 
+any_decimalnumber =0.2,0.4,5.5 #this are float type
+# ture or false are boolean type 
