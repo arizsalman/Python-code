8c515cfaf824eed39ed7fcede07efc7a9030fd50
arizsalman <muhammadarizsalman@gmail.com>
2025-08-13 21:56:58 +0500

    OOP is start 👍

--- OOP/Chapter.1.py
+++ OOP/Chapter.1.py
@@ -1,23 +1,98 @@
-class Chapter:
-    def __init__(self, ChpName, Topic):
-        self.chpName = ChpName
-        self.Topic = Topic
+# class Chapter:
+#     def __init__(self, ChpName, Topic):
+#         self.chpName = ChpName
+#         self.Topic = Topic
 
 
-myChapter = Chapter("Python", "OOP")
-print(myChapter.chpName)
-print(myChapter.Topic)
+# myChapter = Chapter("Python", "OOP")
+# print(myChapter.chpName)
+# print(myChapter.Topic)
 
-myNewChapter = Chapter('React', 'Function')
-print(myNewChapter.Topic)
+# myNewChapter = Chapter('React', 'Function')
+# print(myNewChapter.Topic)
+
+
+# class Car:
+#     def __init__(self, Name, Modal):
+#         self.Name = Name
+#         self.Modal = Modal
+
+
+# myCar = Car('Toyota', 'Carolla')
+# print(myCar.Name)
+# print(myCar.Modal)
+
+
+# class Family:
+#     def __init__(self, fullName, FaName):
+#         self.fuName = fullName
+#         self.FaName = FaName
+
+#     def Full_Family(self):
+#         return f'{self.fuName} {self.FaName}'
+
+
+# myFamily = Family('Ariz', 'Salman')
+# print(myFamily.fuName)
+# print(myFamily.FaName)
+# print(myFamily.Full_Family())
+
+
+# class OurColor:
+#     def __init__(self, color1, color2):
+#         self.color1 = color1
+#         self.color2 = color2
+
+#     def Newcolor(self):
+#         return f'{self.color1}  {self.color2}'
+
+
+# OwnColor = OurColor('Bule', 'Yellow')
+# print(OwnColor.color1)
+# print(OwnColor.color2)
+# print(OwnColor.Newcolor())
+
+
+# class Myform:
+#     def __init__(self, form1, form2, form3):
+#         self.form1 = form1
+#         self.form2 = form2
+#         self.form3 = form3
+
+#     def MargeForm(self):
+#         return f'{self.form1} {self.form3} {self.form2}'
+
+
+# NewFrom = Myform('Hello Wrold', 'Hi there', 'Bye there')
+# print(NewFrom.form1)
+# print(NewFrom.form2)
+# print(NewFrom.form3)
+# print(NewFrom.MargeForm())
 
 
 class Car:
-    def __init__(self, Name, Modal):
-        self.Name = Name
-        self.Modal = Modal
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def Display_info(self):
+        return f' {self.brand} {self.model}'
 
 
-myCar = Car('Toyota', 'Carolla')
-print(myCar.Name)
-print(myCar.Modal)
+class Eletric_Car(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+    def Display_info(self):
+        return f'{super().Display_info()}with Battery_size .{self.battery_size}'
+
+
+MyElectircCar = Eletric_Car('Tesla', 'Js17Sa', '100KHV')
+print(MyElectircCar.Display_info())
+
+
+MyCar = Car('Ferarri', 'E_Class')
+print(MyCar.brand)
+print(MyCar.model)
+print(MyCar.Display_info())
--- Quiz_app.py
+++ Quiz_app.py
@@ -42,124 +42,149 @@
 # print(f"\n Your finall score is : {Update}/{len(mcqs)}")
 
 
-mcc = [
-    {
-        'question': "1) What is a dictionary in Python?",
-        'options': [
-            "A) An ordered collection of elements",
-            "B) An unordered collection of key-value pairs",
-            "C) A list of items in sequence",
-            "D) A tuple with fixed values"
-        ],
-        'answer': "B"
-    },
-    {
-        'question': "2) How do you create an empty dictionary?",
-        'options': [
-            "A) dict = {}",
-            "B) dict = []",
-            "C) dict = ()",
-            "D) dict = ''"
-        ],
-        'answer': "A"
-    },
-    {
-        'question': "3) What will `my_dict['name']` do?",
-        'options': [
-            "A) Add a new key",
-            "B) Delete a key",
-            "C) Get the value for key 'name'",
-            "D) Change the dictionary type"
-        ],
-        'answer': "C"
-    },
-    {
-        'question': "4) Which method is used to get all keys from a dictionary?",
-        'options': [
-            "A) values()",
-            "B) keys()",
-            "C) items()",
-            "D) get()"
-        ],
-        'answer': "B"
-    },
-    {
-        'question': "5) What is the output of `len({'a':1,'b':2,'c':3})`?",
-        'options': [
-            "A) 3",
-            "B) 6",
-            "C) 2",
-            "D) Error"
-        ],
-        'answer': "A"
-    },
-    {
-        'question': "6) Which method removes a key from a dictionary?",
-        'options': [
-            "A) delete()",
-            "B) pop()",
-            "C) remove()",
-            "D) discard()"
-        ],
-        'answer': "B"
-    },
-    {
-        'question': "7) What will happen if you access a non-existent key using `my_dict['key']`?",
-        'options': [
-            "A) Returns None",
-            "B) Returns False",
-            "C) Raises KeyError",
-            "D) Creates the key automatically"
-        ],
-        'answer': "C"
-    },
-    {
-        'question': "8) What does `my_dict.get('key', 'default')` do?",
-        'options': [
-            "A) Removes the key",
-            "B) Returns value if key exists, else 'default'",
-            "C) Always returns 'default'",
-            "D) Updates the key"
-        ],
-        'answer': "B"
-    },
-    {
-        'question': "9) Which of these can be used as dictionary keys?",
-        'options': [
-            "A) Lists",
-            "B) Dictionaries",
-            "C) Tuples",
-            "D) Sets"
-        ],
-        'answer': "C"
-    },
-    {
-        'question': "10) What does `my_dict.clear()` do?",
-        'options': [
-            "A) Deletes the dictionary",
-            "B) Removes all items from dictionary",
-            "C) Creates a copy of dictionary",
-            "D) Sorts the dictionary"
-        ],
-        'answer': "B"
-    }
-]
+# mcc = [
+#     {
+#         'question': "1) What is a dictionary in Python?",
+#         'options': [
+#             "A) An ordered collection of elements",
+#             "B) An unordered collection of key-value pairs",
+#             "C) A list of items in sequence",
+#             "D) A tuple with fixed values"
+#         ],
+#         'answer': "B"
+#     },
+#     {
+#         'question': "2) How do you create an empty dictionary?",
+#         'options': [
+#             "A) dict = {}",
+#             "B) dict = []",
+#             "C) dict = ()",
+#             "D) dict = ''"
+#         ],
+#         'answer': "A"
+#     },
+#     {
+#         'question': "3) What will `my_dict['name']` do?",
+#         'options': [
+#             "A) Add a new key",
+#             "B) Delete a key",
+#             "C) Get the value for key 'name'",
+#             "D) Change the dictionary type"
+#         ],
+#         'answer': "C"
+#     },
+#     {
+#         'question': "4) Which method is used to get all keys from a dictionary?",
+#         'options': [
+#             "A) values()",
+#             "B) keys()",
+#             "C) items()",
+#             "D) get()"
+#         ],
+#         'answer': "B"
+#     },
+#     {
+#         'question': "5) What is the output of `len({'a':1,'b':2,'c':3})`?",
+#         'options': [
+#             "A) 3",
+#             "B) 6",
+#             "C) 2",
+#             "D) Error"
+#         ],
+#         'answer': "A"
+#     },
+#     {
+#         'question': "6) Which method removes a key from a dictionary?",
+#         'options': [
+#             "A) delete()",
+#             "B) pop()",
+#             "C) remove()",
+#             "D) discard()"
+#         ],
+#         'answer': "B"
+#     },
+#     {
+#         'question': "7) What will happen if you access a non-existent key using `my_dict['key']`?",
+#         'options': [
+#             "A) Returns None",
+#             "B) Returns False",
+#             "C) Raises KeyError",
+#             "D) Creates the key automatically"
+#         ],
+#         'answer': "C"
+#     },
+#     {
+#         'question': "8) What does `my_dict.get('key', 'default')` do?",
+#         'options': [
+#             "A) Removes the key",
+#             "B) Returns value if key exists, else 'default'",
+#             "C) Always returns 'default'",
+#             "D) Updates the key"
+#         ],
+#         'answer': "B"
+#     },
+#     {
+#         'question': "9) Which of these can be used as dictionary keys?",
+#         'options': [
+#             "A) Lists",
+#             "B) Dictionaries",
+#             "C) Tuples",
+#             "D) Sets"
+#         ],
+#         'answer': "C"
+#     },
+#     {
+#         'question': "10) What does `my_dict.clear()` do?",
+#         'options': [
+#             "A) Deletes the dictionary",
+#             "B) Removes all items from dictionary",
+#             "C) Creates a copy of dictionary",
+#             "D) Sorts the dictionary"
+#         ],
+#         'answer': "B"
+#     }
+# ]
 
 
-socre = 0
-for i, mcq1 in enumerate(mcc, 1):
-    print(f"\n Q{i} {mcq1['question']} ")
+# socre = 0
+# for i, mcq1 in enumerate(mcc, 1):
+#     print(f"\n Q{i} {mcq1['question']} ")
 
-    for option in mcq1['options']:
-        print(option)
+#     for option in mcq1['options']:
+#         print(option)
 
-    user_input = input("Enter the correct answer : ").strip().upper()
+#     user_input = input("Enter the correct answer : ").strip().upper()
 
-    if user_input == mcq1['answer']:
-        print("Correct")
-        socre += 1
-    else:
-        print(f"Wrong ❌ The correct answer was: {mcq1['answer']}")
+#     if user_input == mcq1['answer']:
+#         print("Correct")
+#         socre += 1
+#     else:
+#         print(f"Wrong ❌ The correct answer was: {mcq1['answer']}")
 
 
-print(f"\n Your finall score is : {socre}/{len(mcc)} ")
+# print(f"\n Your finall score is : {socre}/{len(mcc)} ")
+
+info = {
+    "Ariz": 23,
+    "Hamza": 29,
+    "Ali": 19,
+    "Umer": 27,
+
+}
+infoC = {
+    "Ariz": 'Yellow',
+    "Hamza": 'White',
+    "Ali": 'Snow',
+    "Umer": 'blue',
+
+}
+print(f' Ali Age Find And Ali as key to info ..: {info["Ali"]}')
+print(
+    f' info  Find And Total Dictionary Name in List : {list(info.keys())}')
+print(f'infoColour get() student color :{infoC.get("Ali")}')
+info.update({'Aashir': 29, 'gress': 32})
+print(f'Add a New Student :{info}')
+{info.pop("gress")}
+print(f'Remove a gress  Student  in Method pop:{info}')
+infoC["Ali"] = 'YellowWhite'
+print(f'Update the Value of Ali:{infoC}')
