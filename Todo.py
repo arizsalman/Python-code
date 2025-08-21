bddf076f3618111507ecb6753be8ec1c01bb4826
arizsalman <muhammadarizsalman@gmail.com>
2025-08-21 22:27:27 +0500

    Some practice 💯

--- Todo.py
+++ Todo.py
@@ -6,32 +6,38 @@
 # list_todo = str(lists)
 # print(f'Its your Todo list {list_todo}')
 
-# list_Todo = []
+
+# numBer_List = []
 # while True:
-#     to_do = input(" Please Enter item List : and exit type (exit) ")
-#     if to_do.lower() == "exit":
+#     nums = input("Enter your number exit type ok")
+#     if nums.lower().strip() == "ok":
 #         break
 
 #     try:
-#         list_Todo.append(str(to_do))
+#         numBer_List.append(int(nums))
 
 #     except ValueError:
-#         print(f'Please The Valid item ')
+#         print(f"Please Enter your valid number ")
 
-# new_List = list(list_Todo)
-# print(f'Its Your Grocery item :{new_List}')
+#     new_number_List = list(numBer_List)
+#     print(f' YOur Number list is : {new_number_List}')
 
-numBer_List = []
+
+list_Todo = []
 while True:
-    nums = input("Enter your number exit type ok")
-    if nums.lower().strip() == "ok":
+    to_do = input(" Please Enter item List : and exit type (exit) ")
+    if to_do.lower().strip() == "exit":
         break
 
     try:
-        numBer_List.append(int(nums))
-
+        if to_do.isdigit():
+            raise ValueError('Number is not allow in groceory list ')
+        # to_do.strip("") # ye sirf start me or end se space hata he
+        to_do = to_do.replace(" ", "")  # remove all space start end midlle
+        list_Todo.append(to_do)
     except ValueError:
-        print(f"Please Enter your valid number ")
+        print(f'Please The Valid item ')
 
-    new_number_List = list(numBer_List)
-    print(f' YOur Number list is : {new_number_List}')
+new_List = list(list_Todo)
+print(f'Its Your Grocery item :{new_List}')
+print(f'Its Your Grocery item :{len(new_List)}')
--- functionpart2.py
+++ functionpart2.py
@@ -63,14 +63,14 @@
 # print(multy)  # ye tuple me show ho ga ya
 
 
-def read(names, age):
-    return (f'My Name is {names}, My age is {age}')
+# def read(names, age):
+#     return (f'My Name is {names}, My age is {age}')
 
 
-red = read(names="Ariz", age=24)  # Ye best Taarka he
-red1 = read("Ariz", 24)  # Ye bhe atrkeqa he ye best nahe he
-print(red)
-print(red1)
+# red = read(names="Ariz", age=24)  # Ye best Taarka he
+# red1 = read("Ariz", 24)  # Ye bhe atrkeqa he ye best nahe he
+# print(red)
+# print(red1)
 
 '''Agar hum function ke ander  gobal value ko used karna cahe tu {global} ka keyword used kare gae'''
 
@@ -100,15 +100,92 @@ print(red1)
 # print(pN)
 
 
-def primes(nums):
-    if nums < 2:
-        return f'Prime Number is Greater than {nums} '
-    for i in range(2, (int(nums**0.5)+1)):
-        if nums % i == 0:
-            return f' Not a Prime Number : {nums}'
-    return f' Your number is a Prime number {nums}'
+# def primes(nums):
+#     if nums < 2:
+#         return f'Prime Number is Greater than {nums} '
+#     for i in range(2, (int(nums**0.5)+1)):
+#         if nums % i == 0:
+#             return f' Not a Prime Number : {nums}'
+#     return f' Your number is a Prime number {nums}'
 
 
-nus = int(input('Enter your number and see its a prime number is not '))
-p_N = primes(nus)
-print(p_N)
+# nus = int(input('Enter your number and see its a prime number is not '))
+# p_N = primes(nus)
+# print(p_N)
+
+"""Function likho jo ek string le aur uska reverse return kare."""
+""" {MAIN POINT Her kam karne ke buhat se point hote he see tarha reverse Kene ke 3 and zada terqe he }"""
+
+''' AK Ye Tareka he '''
+
+"""  [:: -1] 
+first pe start  stop  step like kya karne he add etc
+[start:stop:step ]
+"""
+
+# def reverseing(rev):
+#     return rev[:: -1]
+
+# res1 = reverseing("Ariz").upper()
+# print(res1)
+
+"""Ak ye tareka he """
+""" .join() me jo bolo ge kar dee ga  agar ap space hatne ka kaho ge space ahta de ga agar ap kaho ge space hata ke {?>@#$$} koj be add kar do ye kar de ga or jane list,  string , tuple , any method ye mala deta he like this  words = ["Ariz", "Salman", "Python"]
+      result = " ".join(words)
+      print(result)
+      Ariz Salman Pythonn  (ye result he )
+ """
+
+
+# def rever_se(re_v):
+#     return " ? ".join(reversed(re_v))
+
+
+# revd = rever_se("ARIZ")
+# print(revd)
+
+"""Function likho jo ek list me se maximum number return kare (without using max())."""
+
+
+# def func(nums):
+#     maximum = nums[0]
+#     for num in nums:
+#         if num > maximum:
+#             maximum = num
+#     return maximum
+
+
+# print(f'{func([1, 2, 3, 4, 5, 6, 7, 7, 9])}')
+
+
+"""Function likho jo ek list me se maximum number return kare (without using max()) is me hum ak chez add kare ge hum user se number le ge
+us list me ham dekhe ge ke kon sa number bara bhe  ."""
+
+
+def func():
+    big_list = []
+    while True:
+        user_num = input('Enter your number end exit type "done"')
+        if user_num.lower().strip() == "done":
+            break
+        try:
+            num = int(user_num)  # ye string se integer pe kare ga
+            big_list.append(num)
+        except ValueError:
+            print(f'Please enter a valid number ')
+
+        print(big_list)
+
+    if not big_list:
+        return " No Number etered"
+
+    maximum = big_list[0]
+    for i in big_list:
+        if i > maximum:
+            maximum = i
+
+    return f" Maximum Number is : {maximum}"
+
+
+big_list = func()
+print(big_list)
